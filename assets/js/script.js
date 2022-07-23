d1711ad08891dce0628f98a130ab3de25f969c96
Oscar Ayala <titoayala@gmail.com>
2022-07-22 23:37:41 -0400

    Agregando funciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,44 @@
-const inputClp = document.querySelector("#inputClp")
+const pesos = document.querySelector("#inputClp")
 const monedaSeleccionada = document.querySelector("#monedaSeleccionada")
 const boton = document.querySelector("#boton")
 const monedaConvertida = document.querySelector("#monedaConvertida")
 const miGrafico = document.querySelector("#miGrafico")
-const apiUrl = "https://mindicador.cl/api";
+const apiUrl = "https://mindicador.cl/api"
 
+async function getMonedas(url) {
+  try {
+    const res = await fetch(url)
+    const monedas = await res.json()
+    return monedas
+  } catch (error) {
+    alert("Error al cargar la API. Favor intente de nuevo!")
+  }
+}
+boton.addEventListener("click", () => {
+    if (pesos.value == "" || pesos.value <= 0) {
+        alert("Ingrese un monto mayor a CERO")
+        pesos.value = ""
+        return
+      }
+      renderConvierteMonedas()
+})
+async function renderConvierteMonedas() {
+    try {
+      const monedas = await getMonedas(apiUrl);
+      let tipoMoneda = "" //SE DEFINE PARA USARLA EN FORMATEO NUMERICO EN LA LINEA 35
+      if (monedas[monedaSeleccionada.value].codigo === "dolar") {
+        tipoMoneda = "USD"
+      } else {
+        tipoMoneda = "EUR"
+      }
+      monedaConvertida.innerHTML = `Resultado: ${new Intl.NumberFormat("en-US", { //SE FORMATEA A TIPO MONEDA
+        style: "currency",
+        currency: tipoMoneda //TIPO DE MONEDA
+      }).format((pesos.value / monedas[monedaSeleccionada.value].valor).toFixed(2))}` //REDONDEA A DOS DECIMALES
+    }
+    catch (error) {
+      alert("Error en carga! Favor intente de nuevo!")
+      pesos.value = ""
+      return
+    }
+}
