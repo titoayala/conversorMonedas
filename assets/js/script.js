c0ce7be3a59c5117e13ba9c7576d85ffa590a96d
Oscar Ayala <titoayala@gmail.com>
2022-07-22 23:14:03 -0400

    Agregando IDs y variables globales a JS

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const inputClp = document.querySelector("#inputClp")
+const monedaSeleccionada = document.querySelector("#monedaSeleccionada")
+const boton = document.querySelector("#boton")
+const monedaConvertida = document.querySelector("#monedaConvertida")
+const miGrafico = document.querySelector("#miGrafico")
+const apiUrl = "https://mindicador.cl/api";
+
